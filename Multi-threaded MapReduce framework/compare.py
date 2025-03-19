7e2bc21ee0872c69df9e3d9b3e6c2a1ee25098b4
YehoanatnEzra <yonzra12@gmail.com>
2025-03-19 14:47:09 +0200

    check tests

--- Multi-threaded MapReduce framework/compare.py
+++ Multi-threaded MapReduce framework/compare.py
@@ -1,10 +1,14 @@
 def compare_files(output_f, expected_f):
 	with open(output_f, 'r') as out_f, open(expected_f, 'r') as exp_f:
-		out_f = open(output_file, 'r')
-		exp_f = open(expected_file, 'r')
-	if out_f== exp_f:
+		out_lines = [line.strip() for line in out_f.readlines()]
+		exp_lines = [line.strip() for line in exp_f.readlines()]
+
+	if out_lines == exp_lines:
 		print( "Test passed!")
 	else:
 		print("Test failed!")
+		for i, (out, exp) in enumerate(zip(out_lines, exp_lines), 1):
+			if out != exp:
+				print(f" Line {i}: Expected '{exp}', but got '{out}'")
 
 compare_files("Multi-threaded MapReduce framework/test1_output", "Multi-threaded MapReduce framework/test1-1_thread_1_expected.txt")
